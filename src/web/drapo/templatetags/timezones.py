826cd5a80610a2e93f3788da5083511c9c7c2ac6
Andrew Gein <andgein@yandex.ru>
2016-07-27 01:48:38 +0300

    Pretty utc offset for timezone

--- src/web/drapo/templatetags/timezones.py
+++ src/web/drapo/templatetags/timezones.py
@@ -9,4 +9,9 @@ register = Library()
 def utcoffset(value):
     # Yeap, it's strange, but tags are so ugly.. So I defined not use value, but get current timezone from utils
     tz = timezone.get_current_timezone()
-    return datetime.datetime.now(tz).utcoffset()
+    utc_offset = datetime.datetime.now(tz).utcoffset()
+
+    minutes = (utc_offset.days * 24 * 60) + (utc_offset.seconds / 60)
+    if minutes == 0:
+        return ''
+    return '(UTC%+03i:%02i)' % divmod(minutes, 60)
