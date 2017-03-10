83ea611005651d5d7c4dffe22a3e1a6071990d06
Alex Ankudinov <xelez0@gmail.com>
2017-03-10 21:13:37 +0500

    Add command to import participants

---
+++ src/web/contests/management/commands/addparticipants.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand, CommandError
+from users.models import User
+from contests.models import Contest, IndividualParticipant
+
+import json
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('contest_id', type=int)
+        parser.add_argument('filename', type=str)
+
+    def add_user(self):
+        user = User.objects.create_user(
+        )
+        user.save()
+
+    def handle(self, *args, **options):
+        contest_id = options['contest_id']
+        try:
+            contest = Contest.objects.get(pk=contest_id)
+        except Contest.DoesNotExist:
+            raise CommandError('Contest "%s" does not exist' % contest_id)
+
+        with open(options['filename']) as file:
+            participants = json.load(file)
+
+        for p in participants:
+            user = User.objects.create_user(
+                username=p['username'],
+                email=p['email'],
+                first_name=p['first_name'],
+                last_name=p['last_name'],
+                password=p['password']
+            )
+            user.save()
+
+            IndividualParticipant(
+                contest=contest,
+                user=user,
+                is_approved=True,
+            ).save()
+
+            self.stdout.write(self.style.SUCCESS('Registered user %s to contest' % p['username']))
