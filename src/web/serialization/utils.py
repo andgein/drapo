4ec3280bdadbf056c7be3e6d0f644e8f18ec76b1
Mik Vyatskov <crassirostris@yandex.com>
2017-04-13 18:52:05 +0200

    More verbose error reporting

--- src/web/serialization/utils.py
+++ src/web/serialization/utils.py
@@ -5,7 +5,7 @@ import yaml
 
 def load_yaml_from_file(path):
     if path is None or not os.path.isfile(path):
-        raise Exception('Should specify an existing file with a spec')
+        raise Exception('Should specify an existing file with a spec, given "%s"' % path)
 
     with open(path, 'rt', encoding='utf-8') as f:
         yaml_data = f.read()
