4871341f32b4a7dc02fe74e656e50679c9ebb39e
mpivko <mpivko@yandex.ru>
2018-02-18 12:13:42 +0000

    nginx in docker-compose

--- docker/web/docker-entrypoint.sh
+++ docker/web/docker-entrypoint.sh
@@ -5,8 +5,6 @@ dockerize -wait tcp://db:5432 -timeout 1m
 
 # Fix permissions
 chown www-data:www-data /upload
-chown -R root:root /root/.ssh
-chmod 0600 /root/.ssh/id_rsa
 
 case "$1" in
 "manage.py")
